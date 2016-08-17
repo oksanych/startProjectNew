135bfb1142131417c93dbd0996728b5509aa6e33
oksanych <a.oksanych.90@gmail.com>
2016-08-17 11:12:32 +0300

    Modified start project files.

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,24 @@ const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 const uglify = require('gulp-uglify');
 const uncss = require('gulp-uncss');
+const svgSprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
 const browserSync = require('browser-sync').create();
 
 //Path
 //--------------------------
 var path = {
   src: { //Where to get the files
-    html: 'dev/html/pages/*.html',
+    html: 'dev/html/pages/[^_]*.html',
     htmlAll: 'dev/html/**/*.html',
     style: 'dev/scss/main.scss',
-    js: 'dev/js/main.js',
-    img: 'dev/img/**/*.*',
+    js: 'dev/js/*.js',
+    img: 'dev/img/img/**/*.*',
+    imgSVG: 'dev/img/svg-icons/*.svg',
+    svgStyleTemplate: 'dev/scss/svg-sprites/_svg-icons-template.scss',
+    svgStyle: '../../../../dev/scss/svg-sprites/_svg-icons.scss',
     fonts: 'dev/fonts/**/*.*'
   },
   build: { //Where to put the files after assembly
@@ -34,14 +41,16 @@ var path = {
     style: 'app/css/',
     js: 'app/js/',
     img: 'app/img/',
+    imgSVG: 'app/img/svg-icons/',
     fonts: 'app/fonts/'
   },
   watch: { //The change which files we want to observe
-      html: 'dev/html/**/*.html',
-      js: 'dev/js/**/*.js',
-      style: 'dev/scss/**/*.scss',
-      img: 'dev/img/**/*.*',
-      fonts: 'dev/fonts/**/*.*'
+    html: 'dev/html/**/*.html',
+    js: 'dev/js/**/*.js',
+    style: 'dev/scss/**/*.scss',
+    img: 'dev/img/**/[^_]*.*',
+    imgSVG: 'dev/img/svg-icons/*.svg',
+    fonts: 'dev/fonts/**/*.*'
   },
   app:{
     app: 'app'
@@ -54,6 +63,7 @@ const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'developm
 //--------------------------
 gulp.task('html', function() {
     return gulp.src(path.src.html)
+          .pipe(plumber())
           .pipe(rigger())
           .pipe(gulp.dest(path.build.html));
 });
@@ -72,7 +82,7 @@ gulp.task('style', function() {
             };
           }))
           .pipe(autoprefixer({
-            browsers: ['last 2 versions']
+            browsers: ['last 2 versions', 'ie 11']
           }))
           .pipe(gulpIf(!isDevelopment, uncss({
             html: [path.src.htmlAll]
@@ -86,6 +96,7 @@ gulp.task('style', function() {
 //--------------------------
 gulp.task('js', function() {
     return gulp.src(path.src.js)
+          .pipe(plumber())
           .pipe(rigger())
           .pipe(gulpIf(isDevelopment, sourcemaps.init()))
           .pipe(gulpIf(!isDevelopment, uglify()))
@@ -107,6 +118,44 @@ gulp.task('images', function() {
           .pipe(gulp.dest(path.build.img));
 });
 
+// SVG icon sprite
+//--------------------------
+gulp.task('svg', function () {
+    return gulp.src(path.src.imgSVG, {since: gulp.lastRun('svg')})
+        // minify svg
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        // remove all fill, style and stroke declarations in out shapes
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        // cheerio plugin create unnecessary string '&gt;', so replace it.
+        .pipe(replace('&gt', '>'))
+        // build svg sprite
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: '../svg-icons.svg',
+                    render: {
+                        scss: {
+                            dest: path.src.svgStyle,
+                            template: path.src.svgStyleTemplate
+                        }
+                    }
+                }
+            }
+        }))
+        .pipe(gulp.dest(path.build.imgSVG))
+});
+
 // Font
 //--------------------------
 gulp.task('fonts', function() {
@@ -125,17 +174,18 @@ gulp.task('clean', function() {
 //--------------------------
 gulp.task('build', gulp.series(
   'clean',
-  gulp.parallel('html', 'style', 'js', 'images', 'fonts'))
+  gulp.parallel('html', 'style', 'js', 'images', 'svg','fonts'))
 );
 
 // Watch
 //--------------------------
-gulp.task('watch', function() {
-  gulp.watch(path.src.html, gulp.series('html'));
-  gulp.watch(path.src.style, gulp.series('style'));
-  gulp.watch(path.src.js, gulp.series('js'));
-  gulp.watch(path.src.img, gulp.series('images'));
-  gulp.watch(path.src.fonts, gulp.series('fonts'));
+gulp.task('watch', function () {
+    gulp.watch(path.watch.html, gulp.series('html'));
+    gulp.watch(path.watch.style, gulp.series('style'));
+    gulp.watch(path.watch.js, gulp.series('js'));
+    gulp.watch(path.watch.img, gulp.series('images'));
+    gulp.watch(path.watch.svg, gulp.series('svg'));
+    gulp.watch(path.watch.fonts, gulp.series('fonts'));
 });
 
 // Browser-sync
@@ -143,9 +193,9 @@ gulp.task('watch', function() {
 gulp.task('serve', function() {
   browserSync.init({
     server: {
-        baseDir: './' + path.app.app
+        baseDir: './app'
     },
-    startPath: "/html",
+    startPath: "/html/"
   });
 
   browserSync.watch(path.app.app).on('change', browserSync.reload);
